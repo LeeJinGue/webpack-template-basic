ec827185b4715883a7400f9403a72cb837084055
LeeJinGue <hippo9851@gmail.com>
2022-07-25 16:27:27 +0900

    add: project first commit

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const HtmlPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+module.exports = {
+  // 파일을 읽기 시작하는 진입점 설정
+  entry: './js/main.js',
+
+  // 결과물을 반환하는 설정
+  output: {
+
+    clean: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s?css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'postcss-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.js$/,
+        use: [
+          'babel-loader'
+        ]
+      }
+    ]
+  },
+  // 번들링 후 결과물의 처리 방식 등 다양한 플러그인들을 설정
+  plugins: [
+    new HtmlPlugin({
+      template: './index.html'
+    }),
+    // new CopyPlugin({
+    //   patterns: [
+    //     { from: 'static'}
+    //   ]
+    // })
+  ],
+  devServer: {
+    host: 'localhost'
+  }
+}
